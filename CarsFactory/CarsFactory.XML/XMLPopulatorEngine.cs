f9484d01574dba0fd345e8b9c766aff7c3f562db
Merhatt <merhat1998@gmail.com>
2016-10-30 20:50:02 +0200

    Bug fixed

--- CarsFactory/CarsFactory.XML/XMLPopulatorEngine.cs
+++ CarsFactory/CarsFactory.XML/XMLPopulatorEngine.cs
@@ -14,6 +14,7 @@ namespace CarsFactory.XML
         private const string XmlLocation = "../../../CarsFactoryXML.xml";
         private ICarsFactoryDbContext dbContext;
 
+
         public XMLPopulatorEngine(ICarsFactoryDbContext dbContext)
         {
             this.dbContext = dbContext;
